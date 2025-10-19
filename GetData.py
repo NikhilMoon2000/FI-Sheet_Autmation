d993848bd6d6675205ae25b3ec361a160699bc7e
NikhilMoon2000 <nikhilmoon2000@gamil.com>
2025-10-19 08:57:46 +0530

    pushing python extractor code

---
+++ GetData.py
@@ -0,0 +1,27 @@
+import requests
+
+from bs4 import BeautifulSoup 
+
+
+baseurl='https://www.nseindia.com/'
+
+headerColle={
+    'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070213 BonEcho/2.0.0.2pre'
+}
+
+params = {
+    "symbol": "ADANIPORTS"
+}
+
+
+print('test1')
+r=requests.get(baseurl,headers=headerColle)
+print('test2')
+
+
+# print(r.content)
+
+soup=BeautifulSoup(r.content,features="lxml")
+# print(soup)
+
+print(soup.find(class_='stockPreviousClose'))
